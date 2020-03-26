b5a5c44c7df00f3e72368b28944732d154080ff8
Jace Whitten <jace.whitten@gmail.com>
2020-03-26 16:08:43 -0700

    Update to layout

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue';
 import router from './router';
 
 import Buefy from 'buefy';
-import 'buefy/lib/buefy.css';
-import 'ionicons/dist/css/ionicons.min.css';
+import 'buefy/dist/buefy.css';
 
 Vue.use(Buefy);
 
--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <HelloFuckingUglyWorld msg="THiS iS NnoT iT"></HelloFuckingUglyWorld>
+    <HelloFuckingUglyWorld msg="This is not it..."></HelloFuckingUglyWorld>
   </div>
 </template>
 
@@ -14,7 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
