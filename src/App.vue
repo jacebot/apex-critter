71e37ab7a1ac75a342acce1c49da502a41127c7a
Jace Whitten <jace.whitten@gmail.com>
2020-03-27 15:50:42 -0700

    Add page transitions

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
   <div id="app">
     <TopNavbar v-if="isNotRoot"></TopNavbar>
-
-    <router-view/>
-
+    <vue-page-transition name="overlay-up-full">
+      <router-view />
+    </vue-page-transition>
     <FooterNav :pathCheck="isNotRoot"></FooterNav>
   </div>
 </template>
 
 <script>
-import TopNavbar from "@/components/TopNavbar.vue";
-import FooterNav from "@/components/FooterNav.vue";
+import TopNavbar from '@/components/TopNavbar.vue';
+import FooterNav from '@/components/FooterNav.vue';
 
 export default {
-    computed: {
-      isNotRoot() {
-        if (this.$route.path !== '/') {
-          return true;
-        }
+  computed: {
+    isNotRoot() {
+      if (this.$route.path !== '/') {
+        return true;
       }
-    },
-    components: {
+    }
+  },
+  components: {
     TopNavbar,
     FooterNav
   }
-}
+};
 </script>
 
 
 <style lang="less">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
+import VuePageTransition from 'vue-page-transition';
 import App from './App.vue';
 import router from './router';
+
 
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import 'vue-ionicons/ionicons.css';
 
 Vue.use(Buefy);
+Vue.use(VuePageTransition);
 
 Vue.config.productionTip = false;
 
