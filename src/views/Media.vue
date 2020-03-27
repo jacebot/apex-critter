ab512844e3e4a26ed3751a29dec557d3fadc7265
Jace Whitten <jace.whitten@gmail.com>
2020-03-27 15:37:44 -0700

    Add real content to Media page

--- src/views/Media.vue
+++ src/views/Media.vue
@@ -7,8 +7,15 @@
         <div class="tile">
           <div class="tile is-parent is-vertical">
             <article class="tile is-child notification is-primary">
-              <p class="title">Vertical...</p>
-              <p class="subtitle">Top tile</p>
+              <p class="title">I produce music...</p>
+              <p class="subtitle">and sometimes it sounds great.</p>
+              <small>*An older track... but still checks out.</small>
+              <iframe width="100%"
+                      height="166"
+                      scrolling="no"
+                      frameborder="no"
+                      allow="autoplay"
+                      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/269266531&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
             </article>
           </div>
         </div>
@@ -16,8 +23,8 @@
       <div class="tile is-parent">
         <article class="tile is-child notification is-success">
           <div class="content">
-            <p class="title">Tall tile</p>
-            <p class="subtitle">With even more content</p>
+            <p class="title">Other source's...</p>
+            <p class="subtitle">to enjoy more of my music.</p>
             <div class="content">
               <!-- Content -->
             </div>
@@ -29,17 +36,15 @@
 </template>
 
 <script>
-import Hero from "@/components/Hero/Hero.vue";
+import Hero from '@/components/Hero/Hero.vue';
 
 export default {
-  name: "media",
+  name: 'media',
   components: {
     Hero
   }
-
-}
+};
 </script>
 
 <style>
-
 </style>
