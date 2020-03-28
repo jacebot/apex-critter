4342f9ce3943872429e1aea6e38bd496afc1970b
Jace Whitten <jace.whitten@gmail.com>
2020-03-27 17:49:15 -0700

    Refactor page styles

--- src/views/Vault.vue
+++ src/views/Vault.vue
@@ -3,48 +3,43 @@
     <Hero></Hero>
     
     <div class="tile is-ancestor">
-      <div class="tile is-vertical is-8">
+      <div class="tile is-vertical is-12">
         <div class="tile">
           <div class="tile is-parent is-vertical">
-            <article class="tile is-child notification is-primary">
-              <p class="title">Vertical...</p>
-              <p class="subtitle">Top tile</p>
-            </article>
             <article class="tile is-child notification is-warning">
-              <p class="title">...tiles</p>
-              <p class="subtitle">Bottom tile</p>
+              <p class="title">Music I have produced</p>
+              <p class="subtitle">...through tireless hours and occasionally in no time at all.</p>
+               <ul>
+                <li>
+                  <a href="#">Link to a track</a>
+                </li>
+                <li>
+                  <a href="#">Link to a track</a>
+                </li>
+                <li>
+                  <a href="#">Link to a track</a>
+                </li>
+              </ul>
             </article>
           </div>
           <div class="tile is-parent">
             <article class="tile is-child notification is-info">
-              <p class="title">Middle tile</p>
-              <p class="subtitle">With an image</p>
-              <figure class="image is-4by3">
-                <img src="https://bulma.io/images/placeholders/640x480.png">
-              </figure>
+              <p class="title">Mixes that I have done</p>
+              <p class="subtitle">...with various genres including house, techno and more. </p>
+              <ul>
+                <li>
+                  <a href="#">Link to a mix</a>
+                </li>
+                <li>
+                  <a href="#">Link to a mix</a>
+                </li>
+                <li>
+                  <a href="#">Link to a mix</a>
+                </li>
+              </ul>
             </article>
           </div>
         </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-danger">
-            <p class="title">Wide tile</p>
-            <p class="subtitle">Aligned with the right tile</p>
-            <div class="content">
-              <!-- Content -->
-            </div>
-          </article>
-        </div>
-      </div>
-      <div class="tile is-parent">
-        <article class="tile is-child notification is-success">
-          <div class="content">
-            <p class="title">Tall tile</p>
-            <p class="subtitle">With even more content</p>
-            <div class="content">
-              <!-- Content -->
-            </div>
-          </div>
-        </article>
       </div>
     </div>
   </div>
@@ -61,7 +56,3 @@ export default {
 
 }
 </script>
-
-<style>
-
-</style>
